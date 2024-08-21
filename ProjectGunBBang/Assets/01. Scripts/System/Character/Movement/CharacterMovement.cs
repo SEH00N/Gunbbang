45c3dee2d35af5f72c152b26f5934b65b9de145a
SEH00N <06psh0501@gmail.com>
2024-08-21 11:00:01 +0900

    movement

--- ProjectGunBBang/Assets/01. Scripts/System/Character/Movement/CharacterMovement.cs
+++ ProjectGunBBang/Assets/01. Scripts/System/Character/Movement/CharacterMovement.cs
@@ -6,11 +6,55 @@ namespace GB.Characters
     {
         private UnityEngine.CharacterController characterController = null;
 
+        [SerializeField] float maxSpeed = 10f;
+        [SerializeField] float acceleration = 20f;
+        private float currentSpeed = 0f;
+        
+        private Vector3 moveDirection = Vector3.zero;
+        public Vector3 Velocity { get; private set; } = Vector3.zero;
+
         public override void Init(CharacterController controller)
         {
             base.Init(controller);
 
             characterController = GetComponent<UnityEngine.CharacterController>();
+        }
+
+        private void FixedUpdate()
+        {
+            bool shouldMove = moveDirection.sqrMagnitude > 0;
+            if(shouldMove == false)
+                return;
+
+            currentSpeed += acceleration * Time.fixedDeltaTime;
+            currentSpeed = Mathf.Min(currentSpeed, maxSpeed);
+
+            Velocity = currentSpeed * moveDirection;
+            characterController.Move(Velocity * Time.fixedDeltaTime);
+        }
+
+        public void SetDirection(Vector3 direction)
+        {
+            direction = direction.normalized;
+            
+            if(direction.sqrMagnitude < 0.1f)
+            {
+                currentSpeed = 0f;
+            }
+            else
+            {
+                float theta = Mathf.Acos(Vector3.Dot(moveDirection, direction)) * Mathf.Rad2Deg;
+                bool directionReversed = theta > 90f;
+                if(directionReversed)
+                    currentSpeed = 0f;
+            }
+
+            moveDirection = direction;
+        }
+
+        public void AddDirection(Vector3 direction)
+        {
+            SetDirection(moveDirection + direction);
         }
     }
 }
